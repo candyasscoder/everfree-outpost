fd48341b5974047969577496f6c02259b1941b87
Stuart Pernsteiner <stuart@pernsteiner.org>
2015-03-07 10:35:24 -0800

    add road

--- scripts/structure_items.lua
+++ scripts/structure_items.lua
@@ -67,6 +67,7 @@ add_structure_item('house_wall/corner', 'house_wall/corner/inner/nw')
 add_structure_item('house_wall/tee', 'house_wall/tee/interior/e')
 add_structure_item('house_floor')
 add_structure_item('house_door')
+add_structure_item('road')
 
 
 local function mallet_cycle(base, xs)
