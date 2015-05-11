7edf79440e65b5decc83651f36830c92f3a8f92d
Stuart Pernsteiner <stuart@pernsteiner.org>
2015-05-10 19:03:26 -0700

    don't allow equipping multiple hats

--- scripts/hat.lua
+++ scripts/hat.lua
@@ -1,6 +1,10 @@
 local action = require('outpost.action')
 
 function action.use_item.hat(c, inv)
+    if c:pawn():inventory('ability'):count('ability/remove_hat') > 0 then
+        return
+    end
+
     inv:update('hat', -1)
     c:pawn():inventory('ability'):update('ability/remove_hat', 1)
     c:pawn():update_appearance(0x100, 0x100)
