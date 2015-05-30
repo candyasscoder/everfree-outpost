85220b07934033dfbaec40bec4d3c509a5c0acdc
Stuart Pernsteiner <stuart@pernsteiner.org>
2015-05-30 07:28:53 -0700

    teleporter networks

--- client/js/ui/dialogs.js
+++ client/js/ui/dialogs.js
@@ -36,6 +36,75 @@ SignTextDialog.prototype.submit = function() {
 };
 
 
+/** @constructor */
+function TeleportSetupDialog(parts) {
+    var dom_parts = util.templateParts('teleport-setup', {});
+
+    var this_ = this;
+
+    var submit = new widget.Button(dom_parts['submit']);
+    submit.onclick = function() { this_.submit(); };
+    var cancel = new widget.Button(dom_parts['cancel']);
+    cancel.onclick = function() { this_.cancel(); };
+    var buttons = new widget.SimpleList(dom_parts['buttons'], [submit, cancel],
+            ['move_left', 'move_right']);
+
+    this.name_input = new widget.TextField(dom_parts['name']);
+    this.network_input = new widget.TextField(dom_parts['network']);
+
+    var name_row = new widget.Container(dom_parts['name-row'], this.name_input);
+    var network_row = new widget.Container(dom_parts['network-row'], this.network_input);
+
+    var main = new widget.SimpleList(dom_parts['top'],
+            [name_row, network_row, buttons]);
+    widget.Form.call(this, main);
+}
+TeleportSetupDialog.prototype = Object.create(widget.Form.prototype);
+TeleportSetupDialog.prototype.constructor = TeleportSetupDialog;
+
+TeleportSetupDialog.prototype.submit = function() {
+    if (this.onsubmit != null) {
+        this.onsubmit({
+            'name': this.name_input.dom.value,
+            'network': this.network_input.dom.value,
+        });
+    }
+};
+
+
+/** @constructor */
+function TeleportDestDialog(parts) {
+    var dests = parts['dests'];
+    dests.sort();
+
+    var this_ = this;
+    var items = [];
+    var list_elt = util.element('div', ['list']);
+    for (var i = 0; i < dests.length; ++i) {
+        var elt = util.element('div', ['list-item', 'text=' + dests[i]], list_elt);
+        items[i] = new widget.Button(elt);
+        items[i].value = dests[i];
+        items[i].onclick = function() { this_.submit(); };
+    }
+    this.list = new widget.SimpleList(list_elt, items);
+
+    var dom = util.fromTemplate('teleport-dest', {'list': this.list.dom});
+    widget.Form.call(this, this.list, dom);
+}
+TeleportDestDialog.prototype = Object.create(widget.Form.prototype);
+TeleportDestDialog.prototype.constructor = TeleportDestDialog;
+
+TeleportDestDialog.prototype.submit = function() {
+    if (this.onsubmit != null) {
+        this.onsubmit({
+            'dest': this.list.selection().value,
+        });
+    }
+};
+
+
 exports.DIALOG_TYPES = [
     SignTextDialog,
+    TeleportSetupDialog,
+    TeleportDestDialog,
 ];
--- scripts/teleporter.lua
+++ scripts/teleporter.lua
@@ -2,24 +2,116 @@ local action = require('outpost.action')
 local structure_items = require('structure_items')
 local tools = require('tools')
 
-function action.use.teleporter(c, s)
-    c:pawn():teleport(s:extra().destination)
+
+local TELEPORT_SETUP_DIALOG_ID = 1
+local TELEPORT_DEST_DIALOG_ID = 2
+
+function action.use.teleporter(c, s, args)
+    if args == nil then
+        local dests = ExtraArg.list()
+        for k,_ in pairs(net_get_table(s:extra().network)) do
+            dests:push(ExtraArg.str(k))
+        end
+
+        local parts = ExtraArg.map()
+        parts:set('dests', dests)
+
+        c:get_interact_args(TELEPORT_DEST_DIALOG_ID, parts)
+    else
+        local network = s:extra().network
+        local dest_name = args:get('dest'):as_str()
+
+        local dest_pos = net_get_dest(network, dest_name)
+        c:pawn():teleport(dest_pos)
+    end
 end
 
-function action.use_item.teleporter(c, inv)
+function action.use_item.teleporter(c, inv, args)
     if not check_forest(c) then return end
 
-    local home = c:extra().home_pos
-    if home == nil then
-        c:send_message('Must /sethome before placing teleporter')
-        return
+    if args == nil then
+        local id = c:world():item_name_to_id('teleporter')
+        local parts = ExtraArg.map()
+        c:get_use_item_args(id, TELEPORT_SETUP_DIALOG_ID, parts)
+    else
+        local name = args:get('name'):as_str()
+        local network = args:get('network'):as_str()
+
+        ok, err = net_register(network, name, c:pawn():pos())
+        if not ok then
+            c:send_message(err)
+            return
+        end
+
+        local s = structure_items.use_item(c, inv, 'teleporter', 'teleporter')
+        if s == nil then
+            net_deregister(network, name)
+            return
+        end
+
+        s:extra().name = name
+        s:extra().network = network
     end
-    local s = structure_items.use_item(c, inv, 'teleporter', 'teleporter')
-    s:extra().destination = home
 end
 
 function tools.handler.pick.teleporter(c, s, inv)
-    structure_items.use_structure(c, s, 'teleporter')
+    -- `s` will be destroyed if `use_structure` succeeds, so get its name and
+    -- network before that happens.
+    local name = s:extra().name
+    local network = s:extra().network
+    if structure_items.use_structure(c, s, 'teleporter') then
+        net_deregister(network, name)
+    end
+end
+
+
+function net_register(net, name, pos)
+    local table = World.get():extra().teleport_networks
+
+    if table == nil then
+        table = {}
+        World.get():extra().teleport_networks = table
+    end
+
+    if table[net] == nil then
+        table[net] = {}
+    end
+    local net_table = table[net]
+
+    if net_table[name] ~= nil then
+        return false, 'That name is already in use.'
+    else
+        net_table[name] = pos
+        return true
+    end
+end
+
+function is_empty(t)
+    for k,v in pairs(t) do
+        return false
+    end
+    return true
+end
+
+function net_deregister(net, name)
+    local table = World.get():extra().teleport_networks
+    local net_table = table[net]
+    net_table[name] = nil
+    if is_empty(net_table) then
+        table[net] = nil
+    end
+end
+
+function net_get_table(net)
+    return World.get():extra().teleport_networks[net]
+end
+
+function net_get_dest(net, name)
+    local net_table = net_get_table(net)
+    if net_table == nil then
+        return nil
+    end
+    return net_table[name]
 end
 
 
