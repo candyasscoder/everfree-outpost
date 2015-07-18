704bcbf686a14c6b453111e1dc3d651340298fa1
Stuart Pernsteiner <stuart@pernsteiner.org>
2015-07-18 08:14:10 -0700

    add missing editor_anim field to ExtraDefs

--- client/js/data/extras.js
+++ client/js/data/extras.js
@@ -2,12 +2,14 @@
 function ExtraDefsClass() {
     this.anim_dir_table = null;
     this.default_anim = null;
+    this.editor_anim = null;
     this.physics_anim_table = null;
 }
 
 ExtraDefsClass.prototype.init = function(info) {
     this.anim_dir_table = info['anim_dir_table'];
     this.default_anim = info['default_anim'];
+    this.editor_anim = info['editor_anim'];
     this.physics_anim_table = info['physics_anim_table'];
 };
 
