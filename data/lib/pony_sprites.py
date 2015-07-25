64d0eef84ccaf45e9ccb5e7a3b816195d073ee1c
Stuart Pernsteiner <stuart@pernsteiner.org>
2015-07-24 20:26:14 -0700

    new system for generating hat sheets

--- data/lib/pony_sprites.py
+++ data/lib/pony_sprites.py
@@ -1,5 +1,7 @@
+import os
+
 from ...core.builder import *
-from ...core.images import loader
+from ...core.images import load
 from ...core.util import cached, err
 
 from PIL import Image
@@ -16,11 +18,19 @@ DIRS = [
         {'idx': 1},
         ]
 
+INV_DIRS = [None] * 5
+for i, info in enumerate(DIRS):
+    if 'mirror' not in info:
+        INV_DIRS[info['idx']] = i
+
+
 MOTIONS = [
         {'name': 'stand', 'row': 0, 'base_col': 0, 'len': 1, 'fps': 1},
         {'name': 'walk', 'row': 1, 'base_col': 0, 'len': 6, 'fps': 8},
         {'name': 'run', 'row': 3, 'base_col': 0, 'len': 6, 'fps': 12},
         ]
+
+SPRITE_SIZE = (96, 96)
 
 @cached
 def get_anim_group():
@@ -36,3 +46,90 @@ def get_anim_group():
                         '%s-%d' % (m['name'], mirror))
     g.finish()
     return g.unwrap()
+
+def sheets_from_parts(group, parts, size):
+    w,h = size
+    sheets = [Image.new('RGBA', (sw * w, sh * h)) for (sw, sh) in group.sheet_sizes]
+    for name, img in parts.items():
+        anim = group.anims.get(name)
+        if anim is None:
+            util.err('no animation %r in group %r' % (name, group.name))
+            continue
+
+        cur_sheet = sheets[anim.sheet]
+        x,y = anim.offset
+        cur_sheet.paste(img, (x * w, y * h))
+    return sheets
+
+def find_box_containing(alpha, pos):
+    if alpha.getpixel(pos) == 0:
+        return None
+
+    x, y = pos
+    while y > 0 and alpha.getpixel((x, y - 1)) != 0:
+        y -= 1
+    while x > 0 and alpha.getpixel((x - 1, y)) != 0:
+        x -= 1
+    return (x, y)
+
+@cached
+def get_hat_offsets():
+    offsets = {}
+
+    sw, sh = SPRITE_SIZE
+    def get_offset(alpha, x, y):
+        cx = x * sw + sw // 2
+        cy = y * sh + sh // 2
+        px_x, px_y = find_box_containing(alpha, (cx, cy))
+        return (px_x - x * sw, px_y - y * sh)
+
+    for ms in ('mare', 'stallion'):
+        cur_offsets = {}
+        for facing in range(5):
+            path = os.path.join('sprites/base', ms, '%s-%d-hat-box.png' % (ms, facing))
+            image = load(path)
+            alpha = image.split()[3]
+
+            for m in MOTIONS:
+                row = [get_offset(alpha, m['base_col'] + i, m['row']) for i in range(m['len'])]
+                cur_offsets['%s-%d' % (m['name'], facing)] = row
+        offsets[ms] = cur_offsets
+    return offsets
+
+def set_alpha(sheets, alpha):
+    for img in sheets:
+        mask = img.split()[3]
+        mask = mask.point(lambda x: alpha if x == 255 else 0)
+        img.putalpha(mask)
+
+HAT_SIZE = (64, 64)
+
+def mk_hat_sheets(hat_base, group, offsets, depth):
+    parts = {}
+    sw, sh = SPRITE_SIZE
+    hw, hh = HAT_SIZE
+
+    for i in range(5):
+        j = INV_DIRS[i]
+        hat = hat_base.crop((i * hw, 0, (i + 1) * hw, hh))
+
+        for m in MOTIONS:
+            name = m['name']
+            name_i = '%s-%d' % (name, i)
+            name_j = '%s-%d' % (name, j)
+
+            if name_i not in offsets:
+                continue
+
+            frames = len(offsets[name_i])
+            part = Image.new('RGBA', (frames * sw, sh))
+
+            for f in range(frames):
+                ox, oy = offsets[name_i][f]
+                part.paste(hat, (ox + sw * f, oy))
+
+            parts[name_j] = part
+
+    sheets = sheets_from_parts(group, parts, SPRITE_SIZE)
+    set_alpha(sheets, depth)
+    return sheets
--- data/sprites.py
+++ data/sprites.py
@@ -8,27 +8,7 @@ from ..core import util
 from .lib import pony_sprites
 
 
-INV_DIRS = [None] * 5
-for i, info in enumerate(pony_sprites.DIRS):
-    if 'mirror' not in info:
-        INV_DIRS[info['idx']] = i
-
-
 LAYER_NAMES = ('base', 'horn', 'frontwing', 'backwing')
-
-def sheets_from_parts(group, parts, size):
-    w,h = size
-    sheets = [Image.new('RGBA', (sw * w, sh * h)) for (sw, sh) in group.sheet_sizes]
-    for name, img in parts.items():
-        anim = group.anims.get(name)
-        if anim is None:
-            util.err('no animation %r in group %r' % (name, group.name))
-            continue
-
-        cur_sheet = sheets[anim.sheet]
-        x,y = anim.offset
-        cur_sheet.paste(img, (x * w, y * h))
-    return sheets
 
 def mk_layer_sheets():
     mare = loader('sprites/base/mare')
@@ -37,7 +17,7 @@ def mk_layer_sheets():
     for l in LAYER_NAMES:
         parts = {}
         for i in range(5):
-            j = INV_DIRS[i]
+            j = pony_sprites.INV_DIRS[i]
             img = mare('mare-%d-%s.png' % (i, l))
             take = lambda x, y, w: img.crop((x * 96, y * 96, (x + w) * 96, (y + 1) * 96))
 
@@ -45,7 +25,7 @@ def mk_layer_sheets():
             parts['walk-%d' % j] = take(0, 1, 6)
             parts['run-%d' % j] = take(0, 3, 6)
 
-        sheets[l] = sheets_from_parts(pony_sprites.get_anim_group(), parts, (96, 96))
+        sheets[l] = pony_sprites.sheets_from_parts(pony_sprites.get_anim_group(), parts, (96, 96))
 
     return sheets
 
@@ -93,16 +73,13 @@ def mk_hair_sheets(img, depth):
 
     parts = {}
     for i in range(5):
-        j = INV_DIRS[i]
+        j = pony_sprites.INV_DIRS[i]
         parts['stand-%d' % j] = take(i, 0, 1)
         parts['walk-%d' % j] = take(i * 6, 1, 6)
         parts['run-%d' % j] = take(i * 6, 3, 6)
 
-    sheets = sheets_from_parts(pony_sprites.get_anim_group(), parts, (96, 96))
-    for s in sheets:
-        mask = s.split()[3]
-        mask = mask.point(lambda x: depth if x == 255 else 0)
-        s.putalpha(mask)
+    sheets = pony_sprites.sheets_from_parts(pony_sprites.get_anim_group(), parts, (96, 96))
+    pony_sprites.set_alpha(sheets, depth)
     return sheets
 
 
@@ -121,10 +98,13 @@ def init():
             mk_hair_sheets(sprites('maremane1.png'), 120))
     tail = mk_sprite('pony/tail/f/0', group, (96, 96),
             mk_hair_sheets(sprites('maretail1.png'), 120))
-    hat = mk_sprite('pony/equip0/f/0', group, (96, 96),
-            mk_hair_sheets(sprites('equip_f_hat.png'), 130))
 
     mk_attach_slot('eyes', group).add_variant('0', eyes)
     mk_attach_slot('mane', group).add_variant('0', mane)
     mk_attach_slot('tail', group).add_variant('0', tail)
+
+    hat_base = sprites('equipment/witch-hat.png')
+    offsets = pony_sprites.get_hat_offsets()['mare']
+    hat = mk_sprite('pony/equip0/f/0', group, (96, 96),
+            pony_sprites.mk_hat_sheets(hat_base, group, offsets, 130))
     mk_attach_slot('hat', group).add_variant('0', hat)
