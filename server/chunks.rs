d33a6575326ab2c3c10a05af006224e4a21e2c9f
Stuart Pernsteiner <stuart@pernsteiner.org>
2015-08-16 15:35:36 -0700

    restore old chunk loading behavior to fix missing StructureGone messages

--- server/chunks.rs
+++ server/chunks.rs
@@ -118,13 +118,23 @@ pub trait Fragment<'d> {
 
 
 struct Lifecycle {
+    // When client code requests chunk (x,y), we load not only (x,y) but also (x-1,y), (x,y-1), and
+    // (x-1,y-1).  This ensures that every structure overlapping (x,y) is loaded, even those whose
+    // base lies outside the chunk itself.
+    //
+    // To keep track of these internal references, we use two different reference counts.
+    // `user_ref_count` counts the number of external users that have requested to load the chunk.
+    // `ref_count` counts "internal users", which are the four nearby chunks that care about
+    // structures here.
     ref_count: HashMap<(PlaneId, V2), u32>,
+    user_ref_count: HashMap<(PlaneId, V2), u32>,
 }
 
 impl Lifecycle {
     pub fn new() -> Lifecycle {
         Lifecycle {
             ref_count: HashMap::new(),
+            user_ref_count: HashMap::new(),
         }
     }
 
@@ -133,7 +143,7 @@ impl Lifecycle {
                      cpos: V2,
                      mut load: F) -> bool
             where F: FnMut(PlaneId, V2) {
-        let first = match self.ref_count.entry((pid, cpos)) {
+        let first = match self.user_ref_count.entry((pid, cpos)) {
             Vacant(e) => {
                 e.insert(1);
                 debug!("retain: 1 users of {:?} {:?}", pid, cpos);
@@ -147,7 +157,9 @@ impl Lifecycle {
         };
 
         if first {
-            load(pid, cpos);
+            for subpos in Region::around(cpos, 1).points() {
+                self.retain_inner(pid, subpos, &mut load);
+            }
         }
 
         first
@@ -158,7 +170,7 @@ impl Lifecycle {
                       cpos: V2,
                       mut unload: F) -> bool
             where F: FnMut(PlaneId, V2) {
-        let last = if let Occupied(mut e) = self.ref_count.entry((pid, cpos)) {
+        let last = if let Occupied(mut e) = self.user_ref_count.entry((pid, cpos)) {
             *e.get_mut() -= 1;
             debug!("release: {} users of {:?} {:?}", *e.get(), pid, cpos);
             if *e.get() == 0 {
@@ -173,9 +185,55 @@ impl Lifecycle {
         };
 
         if last {
-            unload(pid, cpos);
+            for subpos in Region::around(cpos, 1).points() {
+                self.release_inner(pid, subpos, &mut unload);
+            }
         }
 
         last
+    }
+
+    pub fn retain_inner<F>(&mut self,
+                           pid: PlaneId,
+                           cpos: V2,
+                           load: &mut F)
+            where F: FnMut(PlaneId, V2) {
+        let first = match self.ref_count.entry((pid, cpos)) {
+            Vacant(e) => {
+                e.insert(1);
+                true
+            },
+            Occupied(e) => {
+                *e.into_mut() += 1;
+                false
+            }
+        };
+
+        if first {
+            (*load)(pid, cpos);
+        }
+    }
+
+    pub fn release_inner<F>(&mut self,
+                            pid: PlaneId,
+                            cpos: V2,
+                            unload: &mut F)
+            where F: FnMut(PlaneId, V2) {
+        let last = if let Occupied(mut e) = self.ref_count.entry((pid, cpos)) {
+            *e.get_mut() -= 1;
+            if *e.get() == 0 {
+                e.remove();
+                true
+            } else {
+                false
+            }
+        } else {
+            panic!("tried to release chunk {:?} {:?}, but its ref_count is already zero",
+                   pid, cpos);
+        };
+
+        if last {
+            (*unload)(pid, cpos);
+        }
     }
 }
