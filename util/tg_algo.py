20ff4d665a0d868f2734def7ddce5a26f829822d
Stuart Pernsteiner <stuart@pernsteiner.org>
2015-10-07 06:52:45 -0700

    blob generation prototype

--- util/tg_algo.py
+++ util/tg_algo.py
@@ -4,8 +4,33 @@ import random
 import numpy as np
 from PIL import Image, ImageDraw
 
-from outpost_savegame import V2, V3
+try:
+    from outpost_savegame import V2
+except ImportError:
+    class V2(object):
+        def __init__(self, x, y):
+            self.x = x
+            self.y = y
 
+        def __add__(self, other):
+            return V2(self.x + other.x, self.y + other.y)
+
+        def __sub__(self, other):
+            return V2(self.x - other.x, self.y - other.y)
+
+        def __mul__(self, other):
+            return V2(self.x * other.x, self.y * other.y)
+
+        def __hash__(self):
+            return hash((self.x, self.y))
+
+
+def dist2(a, b):
+    d = a - b
+    return d.x * d.x + d.y * d.y
+
+
+# Poisson disk sampling
 
 class DiskSampler(object):
     def __init__(self, rng, size, spacing):
@@ -23,9 +48,7 @@ class DiskSampler(object):
             return False
 
         for p in self.points:
-            d = pos - p
-            dist2 = d.x * d.x + d.y * d.y
-            if dist2 < self.min2:
+            if dist2(pos, p) < self.min2:
                 return False
         return True
 
@@ -40,8 +63,8 @@ class DiskSampler(object):
                 x = self.rng.randrange(-self.spacing, self.spacing + 1)
                 y = self.rng.randrange(-self.spacing, self.spacing + 1)
                 off = V2(x, y)
-                dist2 = off.x * off.x + off.y * off.y
-                if dist2 >= self.min2 and dist2 <= self.max2:
+                d2 = dist2(V2(0, 0), off)
+                if d2 >= self.min2 and d2 <= self.max2:
                     candidate = pos + off
                     break
             self.try_place(candidate)
@@ -68,10 +91,16 @@ class DiskSampler(object):
         return result
 
 def disk_sample(size, spacing, init_points=[], rng=random):
+    '''Fill a grid of the given `size` with randomly placed points.  Every
+    point is at least `spacing` distance from each of its neighbors.  If
+    `init_points` is provided, each point in that list will be included in the
+    output (assuming no points in the list are closer than `spacing`).'''
     samp = DiskSampler(rng, size, spacing)
     samp.generate(30, init_points)
     return samp.points
 
+
+# Delaunay triangulation.
 
 Triangle = namedtuple('Triangle', ('a', 'b', 'c'))
 
@@ -100,6 +129,10 @@ def circumcircle_contains(tri, p):
     return np.linalg.det(mat) > 0
 
 def triangulate(points):
+    '''Run Delaunay triangulation over a collection of points.  Returns the
+    mesh in adjacency list format (`v2 in result[v1]` iff `v1` and `v2` are
+    connected by an edge in the mesh, where `v1` and `v2` are `V2` instances.
+    '''
     min_x = min(p.x for p in points) - 10
     max_x = max(p.x for p in points) + 10
     min_y = min(p.y for p in points) - 10
@@ -149,19 +182,78 @@ def triangulate(points):
     return edge_map
 
 
+# Main
+
 def main():
-    points = disk_sample(V2(256, 256), 16, rng=random.Random(12345))
+    # Generate the graph.  Place some random points and connect them using
+    # triangles.
+    rng = random.Random(12345)
+    points = disk_sample(V2(256, 256), 8, rng=rng)
     edge_map = triangulate(points)
     edges = list(set((s, t) for s,ts in edge_map.items() for t in ts))
-    img = Image.new('RGBA', (256, 256))
-    d = ImageDraw.Draw(img)
-    for p in points:
-        d.point((p.x, p.y))
-    for s,t in edges:
-        print('  %d,%d - %d,%d' % (s.x, s.y, t.x, t.y))
-        d.line((s.x, s.y, t.x, t.y))
-    img.show()
 
+    # Generate a random blob by choosing vertices from the graph.
+
+    # Dict mapping each vertex position to a count of the number of blob
+    # vertices connected to that vertex.
+    adj_map = defaultdict(lambda: 0)
+    # Set of points chosen to be part of the blob.
+    chosen = set()
+    # Set of all points adjacent to the blob.
+    pending = set()
+
+    # Start with a seed point near the center of the region.
+    center = V2(128, 128)
+    pending.add(sorted(points, key=lambda p: dist2(p, center))[0])
+
+    # Stop after choosing 30 points, or if there are no more points to
+    # consider.
+    while len(pending) > 0 and len(chosen) < 30:
+        # Consider all the adjacent vertices, and maximum number of edges
+        # connecting one of those vertices back to the blob.  Choose a point
+        # that is as well-connected as the maximum.
+        m = max(adj_map[n] for n in pending)
+        highest = list(n for n in pending if adj_map[n] == m)
+        cur = rng.choice(highest)
+
+        # Add the chosen point to the blob.
+        pending.remove(cur)
+        chosen.add(cur)
+
+        # The blob has grown, so update `adj_map` and `pending`.
+        for n in edge_map[cur]:
+            if n not in chosen:
+                adj_map[n] += 1
+                pending.add(n)
+
+
+        # Everything after this is just for producing the images.  It doesn't
+        # affect the result of the algorithm.
+
+        m = max(adj_map[n] for n in pending)
+
+        img = Image.new('RGBA', (256, 256))
+        img.paste('black')
+        d = ImageDraw.Draw(img)
+        for s,t in edges:
+            if s in chosen and t in chosen:
+                fill = 'white'
+            else:
+                fill = (64, 64, 64)
+            d.line((s.x, s.y, t.x, t.y), fill=fill)
+        for p in points:
+            if p in chosen:
+                fill = 'white'
+            elif p in pending:
+                if adj_map[p] == m:
+                    fill = (0, 255, 0)
+                else:
+                    fill = (0, 128, 0)
+            else:
+                continue
+            d.rectangle((p.x - 1, p.y - 1, p.x + 2, p.y + 2), fill=fill)
+        d.text((5, 5), 'improved')
+        img.save('work/improved-%03d.png' % len(chosen))
 
 if __name__ == '__main__':
     main()
