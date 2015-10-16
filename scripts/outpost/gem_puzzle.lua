ad616ca78dc34b1cc633112e7bcbbb784dfa8aea
Stuart Pernsteiner <stuart@pernsteiner.org>
2015-10-16 09:14:22 -0700

    fix gem puzzle scripts

--- scripts/outpost/gem_puzzle.lua
+++ scripts/outpost/gem_puzzle.lua
@@ -2,6 +2,64 @@ local action = require('core.action')
 local util = require('core.util')
 
 local COLORS = { 'red', 'orange', 'yellow', 'green', 'blue', 'purple' }
+local COLOR_VAL = {
+    red = 0,
+    orange = 1,
+    yellow = 2,
+    green = 3,
+    blue = 4,
+    purple = 5,
+    empty = -1,
+}
+
+
+local function puzzle_solved(p)
+    for i = 1, #p.slots do
+        local c = p.slots[i]
+        if c < 0 then
+            return false
+        end
+
+        -- Slots 1/3/... should contain colors 0 (red), 2 (yellow), or 4 (blue)
+        if i % 2 ~= (c + 1) % 2 then
+            return false
+        end
+
+        -- Slots 2/4/... (which contain secondary colors) should match the
+        -- primaries on either side.
+        -- TODO: currently assumes #p.slots is odd
+        if i % 2 == 0 then
+            local a = (c - 1 + 6) % 6
+            local b = (c + 1) % 6
+
+            local l = p.slots[i - 1]
+            local r = p.slots[i + 1]
+
+            if not ((l == a and r == b) or (l == b and r == a)) then
+                return false
+            end
+        end
+    end
+    return true
+end
+
+local function update_puzzle(p, slot, color)
+    p.slots[slot] = COLOR_VAL[color]
+
+    if puzzle_solved(p) then
+        if not p.door_open then
+            door.open(p.door:get())
+            p.door_open = true
+        end
+    else
+        if p.door_open then
+            door.close(p.door:get())
+            p.door_open = false
+        end
+    end
+end
+
+
 
 local ITEM_PREFIX = 'gem/'
 local NORMAL_PREFIX = 'dungeon/gem_slot/normal/'
@@ -48,62 +106,14 @@ local function handle_use_item(c, inv, color)
     update_puzzle(p, s:extra().slot_index, color)
 end
 
-
-local COLOR_VAL = {
-    red = 0,
-    orange = 1,
-    yellow = 2,
-    green = 3,
-    blue = 4,
-    purple = 5,
-    empty = -1,
-}
-
-local function update_puzzle(p, slot, color)
-    p.slots[slot] = COLOR_VAL[color]
-
-    if puzzle_solved(p) then
-        if not p.door_open then
-            door.open(p.door:get())
-            p.door_open = true
-        end
-    else
-        if p.door_open then
-            door.close(p.door:get())
-            p.door_open = false
-        end
+for i, color in ipairs(COLORS) do
+    action.use[FIXED_PREFIX .. color] = handle_use_fixed
+    action.use[NORMAL_PREFIX .. color] = handle_use_normal
+    action.use_item[ITEM_PREFIX .. color] = function(c, inv)
+        handle_use_item(c, inv, color)
     end
 end
 
-local function puzzle_solved(p)
-    for i in 1 .. #p.slots do
-        local c = p.slots[i]
-        if c < 0 then
-            return false
-        end
-
-        -- Slots 1/3/... should contain colors 0 (red), 2 (yellow), or 4 (blue)
-        if i % 2 ~= (c + 1) % 2 then
-            return false
-        end
-
-        -- Slots 2/4/... (which contain secondary colors) should match the
-        -- primaries on either side.
-        -- TODO: currently assumes #p.slots is odd
-        if i % 2 == 0 then
-            local a = (c - 1 + 6) % 6
-            local b = (c + 1) % 6
-
-            local l = p.slots[i - 1]
-            local r = p.slots[i + 1]
-
-            if not ((l == a and r == b) or (l == b and r == a)) then
-                return false
-            end
-        end
-    end
-    return true
-end
 
 
 local function get_puzzle(plane, pid)
@@ -129,8 +139,10 @@ end
 local function apply_gem_puzzle_slot(s, k, v)
     local pid, slot, init = v:match('(.*),(.*),(.*)')
     local p = get_puzzle(s:plane(), pid)
+    slot = 1 + slot
     p.slots[slot] = COLOR_VAL[init]
     s:extra().puzzle_id = pid
+    s:extra().slot_index = slot
 end
 
 local function apply_gem_puzzle_door(s, k, v)
@@ -138,15 +150,6 @@ local function apply_gem_puzzle_door(s, k, v)
     local p = get_puzzle(s:plane(), pid)
     p.door = s:stable_id()
     s:extra().puzzle_id = pid
-end
-
-
-for i, color in ipairs(COLORS) do
-    action.use[FIXED_PREFIX .. color] = handle_use_fixed
-    action.use[NORMAL_PREFIX .. color] = handle_use_normal
-    action.use_item[ITEM_PREFIX .. color] = function(c, inv)
-        handle_use_item(c, inv, color)
-    end
 end
 
 
