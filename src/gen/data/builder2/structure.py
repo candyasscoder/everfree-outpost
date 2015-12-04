79540bb7c3f6c03b3d3ca0ddbd2b4b49e3ef7a2e
Stuart Pernsteiner <stuart@pernsteiner.org>
2015-12-03 19:21:48 -0800

    add multipart support to structure builder

--- src/gen/data/builder2/structure.py
+++ src/gen/data/builder2/structure.py
@@ -1,7 +1,7 @@
 from outpost_data.core import image2
 from outpost_data.core.builder2.base import *
 from outpost_data.core.consts import *
-from outpost_data.core.structure import solid, StructureDef, StaticAnimDef
+from outpost_data.core.structure import solid, StructureDef, StructureDef2, StaticAnimDef
 
 
 DEFAULT_SHAPE = solid(1, 1, 1)
@@ -9,7 +9,7 @@ DEFAULT_SHAPE = solid(1, 1, 1)
 class StructurePrototype(PrototypeBase):
     KIND = 'structure'
     FIELDS = (
-            'image', 'model', 'shape', 'layer',
+            'image', 'model', 'shape', 'layer', 'parts',
             'light_offset', 'light_color', 'light_radius',
             'anim_frames', 'anim_framerate', 'anim_oneshot',
             )
@@ -17,20 +17,31 @@ class StructurePrototype(PrototypeBase):
     def instantiate(self):
         self.name = self.require('name') or '_%x' % id(self)
 
-        if self.require_one('image', 'anim_frames'):
-            img = raw_image(self.image)
-        else:
-            frames = self.anim_frames
-            rate = self.require('anim_framerate', default=1, reason='anim_frames')
-            oneshot = self.anim_oneshot or False
-
-            img = StaticAnimDef(self.anim_frames, self.anim_framerate, self.anim_oneshot)
-
-        model = self.require('model')
         shape = self.require('shape', DEFAULT_SHAPE)
         layer = self.require('layer', 0)
 
-        s = StructureDef(self.name, img, model, shape, layer)
+        if self.require_one('model', 'parts'):
+            if self.require_one('image', 'anim_frames'):
+                img = raw_image(self.image)
+            else:
+                frames = self.anim_frames
+                rate = self.require('anim_framerate', default=1, reason='anim_frames')
+                oneshot = self.anim_oneshot or False
+
+                img = StaticAnimDef(self.anim_frames, self.anim_framerate, self.anim_oneshot)
+            model = self.model
+
+            s = StructureDef(self.name, img, model, shape, layer)
+        else:
+            parts = self.parts
+            self.require_unset('image', 'parts')
+            self.require_unset('anim_frames', 'parts')
+            self.require_unset('anim_framerate', 'parts')
+            self.require_unset('anim_oneshot', 'parts')
+
+            s = StructureDef2(self.name, shape, layer)
+            for m, i in parts:
+                s.add_part(m, i)
 
         pos, color, radius = self.check_group(
                 ('light_offset', 'light_color', 'light_radius'))
@@ -54,6 +65,7 @@ class StructureBuilder(BuilderBase):
     model = dict_modifier('model')
     shape = dict_modifier('shape')
     layer = dict_modifier('layer')
+    parts = dict_modifier('parts')
 
     light_offset = dict_modifier('light_offset')
     light_color = dict_modifier('light_color')
@@ -76,3 +88,14 @@ class StructureBuilder(BuilderBase):
             x.anim_framerate = framerate
             x.anim_oneshot = oneshot
         return self._modify(f)
+
+    def part(self, *args):
+        if len(args) == 1:
+            args, = args
+
+        def f(x, part):
+            if x.parts is None:
+                x.parts = [part]
+            else:
+                x.parts.append(part)
+        self._dict_modify(f, args)
